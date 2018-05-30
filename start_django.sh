417c9cbd8257386c4b80b9544c858846ad4e1ef1
bjgoode <bjgoode@vt.edu>
2018-05-30 09:24:32 -0400

    add workers to gunicorn call

--- start_django.sh
+++ start_django.sh
@@ -4,4 +4,4 @@ su -c "python3.6 manage.py bower install" django
 su -c "python3.6 manage.py collectstatic" django
 su -c "python3.6 manage.py migrate" django
 
-su -c "gunicorn sdal_cln.wsgi:application -b 0.0.0.0:8000" django
+su -c "gunicorn sdal_cln.wsgi:application -b 0.0.0.0:8000 --workers=10" django
